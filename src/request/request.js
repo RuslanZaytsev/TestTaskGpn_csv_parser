b5d51c0fb254a97771712a01a5d260c1ce515bbd
Ruzan <marktt@bk.ru>
2024-07-21 12:46:49 +0300

    create front

---
+++ src/request/request.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const getCSV = async () => {
+  const response = await axios
+    .get("http://localhost:3001/csv")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(new Error(error), "axios error");
+    });
+  return response;
+};
+
+export { getCSV };
